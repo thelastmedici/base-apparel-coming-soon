227e0275ee053f002cb7e725289e9576c9f8a161
thelastmedici <christianopeyemijoshua@gmail.com>
2025-02-14 22:55:14 +0100

    added js file

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello asiwaju")
+const button = document.querySelector('form button');
+
+button.addEventListener('click', function(event) {
+  event.preventDefault();
+  console.log('i got clicked');
+ }
+);
