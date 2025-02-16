9531863043493372e6f3c94bb6a4e80eb3d59c6d
thelastmedici <christianopeyemijoshua@gmail.com>
2025-02-17 00:16:54 +0100

    added functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const button = document.querySelector('form button');
 const errorImage = document.querySelector('form img');
 const errorText = document.querySelector('.error-text');
+const inputField = document.querySelector('form input[type="text"]');
 
 button.addEventListener('click', function(event) {
   event.preventDefault();
@@ -8,5 +9,12 @@ button.addEventListener('click', function(event) {
   if (!email || ! email.includes('@')) {
       errorImage.classList.add('icon-error');
       errorText.style.display = 'block';
+      // inputField.style.display.color = 'red';
+    }
+  else{
+    errorImage.classList.remove('icon-error');
+    errorText.style.display = 'none';
+    inputField.value = ''; // Clear the input field after successful submission
   }
 });
+
